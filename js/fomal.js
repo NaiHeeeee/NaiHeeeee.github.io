fb4b111405a96ea84ff577035223aaeeff734fc3
NaiHeeeee <2794096231@qq.com>
2024-07-28 17:12:41 +0800

    Site updated: 2024-07-28 17:12:40

--- js/fomal.js
+++ js/fomal.js
@@ -108,7 +108,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-  let dist = getDistance(119.71668832, 30.26443504, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+  let dist = getDistance(119.13033832, 29.12672989, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
   let pos = ipLoacation.result.ad_info.nation;
   let ip;
   let posdesc;
