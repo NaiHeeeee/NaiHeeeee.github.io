66fb5e7166bcd71878553482f4ece07eace7949d
NaiHeeeee <2794096231@qq.com>
2025-02-08 20:07:23 +0800

    Site updated: 2025-02-08 20:07:20

--- sw.js
+++ sw.js
@@ -1,93 +1,93 @@
-const CACHE_NAME = 'ICDNCache';
-let cachelist = [];
-self.addEventListener('install', async function (installEvent) {
-    self.skipWaiting();
-    installEvent.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(cachelist);
-            })
-    );
-});
-self.addEventListener('fetch', async event => {
-    try {
-        event.respondWith(handle(event.request))
-    } catch (msg) {
-        event.respondWith(handleerr(event.request, msg))
-    }
-});
-const handleerr = async (req, msg) => {
-    return new Response(`<h1>CDN分流器遇到了致命错误</h1>
-    <b>${msg}</b>`, { headers: { "content-type": "text/html; charset=utf-8" } })
-}
-const lfetch = async (urls, url) => {
-    let controller = new AbortController();
-    const PauseProgress = async (res) => {
-        return new Response(await (res).arrayBuffer(), { status: res.status, headers: res.headers });
-    };
-    if (!Promise.any) {
-        Promise.any = function (promises) {
-            return new Promise((resolve, reject) => {
-                promises = Array.isArray(promises) ? promises : []
-                let len = promises.length
-                let errs = []
-                if (len === 0) return reject(new AggregateError('All promises were rejected'))
-                promises.forEach((promise) => {
-                    promise.then(value => {
-                        resolve(value)
-                    }, err => {
-                        len--
-                        errs.push(err)
-                        if (len === 0) {
-                            reject(new AggregateError(errs))
-                        }
-                    })
-                })
-            })
-        }
-    }
-    return Promise.any(urls.map(urls => {
-        return new Promise((resolve, reject) => {
-            fetch(urls, {
-                signal: controller.signal
-            })
-                .then(PauseProgress)
-                .then(res => {
-                    if (res.status == 200) {
-                        controller.abort();
-                        resolve(res)
-                    } else {
-                        reject(res)
-                    }
-                })
-        })
-    }))
-}
-self.addEventListener('fetch', async event => {
-    event.respondWith(handle(event.request))
-});
-const fullpath = (path) => {
-    path = path.split('?')[0].split('#')[0]
-    if (path.match(/\/$/)) {
-        path += 'index'
-    }
-    if (!path.match(/\.[a-zA-Z]+$/)) {
-        path += '.html'
-    }
-    return path
-}
+// const CACHE_NAME = 'ICDNCache';
+// let cachelist = [];
+// self.addEventListener('install', async function (installEvent) {
+//     self.skipWaiting();
+//     installEvent.waitUntil(
+//         caches.open(CACHE_NAME)
+//             .then(function (cache) {
+//                 console.log('Opened cache');
+//                 return cache.addAll(cachelist);
+//             })
+//     );
+// });
+// self.addEventListener('fetch', async event => {
+//     try {
+//         event.respondWith(handle(event.request))
+//     } catch (msg) {
+//         event.respondWith(handleerr(event.request, msg))
+//     }
+// });
+// const handleerr = async (req, msg) => {
+//     return new Response(`<h1>CDN分流器遇到了致命错误</h1>
+//     <b>${msg}</b>`, { headers: { "content-type": "text/html; charset=utf-8" } })
+// }
+// const lfetch = async (urls, url) => {
+//     let controller = new AbortController();
+//     const PauseProgress = async (res) => {
+//         return new Response(await (res).arrayBuffer(), { status: res.status, headers: res.headers });
+//     };
+//     if (!Promise.any) {
+//         Promise.any = function (promises) {
+//             return new Promise((resolve, reject) => {
+//                 promises = Array.isArray(promises) ? promises : []
+//                 let len = promises.length
+//                 let errs = []
+//                 if (len === 0) return reject(new AggregateError('All promises were rejected'))
+//                 promises.forEach((promise) => {
+//                     promise.then(value => {
+//                         resolve(value)
+//                     }, err => {
+//                         len--
+//                         errs.push(err)
+//                         if (len === 0) {
+//                             reject(new AggregateError(errs))
+//                         }
+//                     })
+//                 })
+//             })
+//         }
+//     }
+//     return Promise.any(urls.map(urls => {
+//         return new Promise((resolve, reject) => {
+//             fetch(urls, {
+//                 signal: controller.signal
+//             })
+//                 .then(PauseProgress)
+//                 .then(res => {
+//                     if (res.status == 200) {
+//                         controller.abort();
+//                         resolve(res)
+//                     } else {
+//                         reject(res)
+//                     }
+//                 })
+//         })
+//     }))
+// }
+// self.addEventListener('fetch', async event => {
+//     event.respondWith(handle(event.request))
+// });
+// const fullpath = (path) => {
+//     path = path.split('?')[0].split('#')[0]
+//     if (path.match(/\/$/)) {
+//         path += 'index'
+//     }
+//     if (!path.match(/\.[a-zA-Z]+$/)) {
+//         path += '.html'
+//     }
+//     return path
+// }
 
-const handle = async(req)=>{
-    const urlStr = req.url
-    const urlObj = new URL(urlStr);
-    const urlPath = urlObj.pathname;
-    const domain = urlObj.hostname;
-    //从这里开始
-    if(domain === "naihee.cn"){//这里写你需要拦截的域名
-        return lfetch(["{https://naiheblog.s3.bitiful.net}"+fullpath(urlPath)])//屏蔽了自带的npm劫持，改为存储桶劫持
-    }
-    else{
-        return fetch(req);
-    }
-}
+// const handle = async(req)=>{
+//     const urlStr = req.url
+//     const urlObj = new URL(urlStr);
+//     const urlPath = urlObj.pathname;
+//     const domain = urlObj.hostname;
+//     //从这里开始
+//     if(domain === "naihee.cn"){//这里写你需要拦截的域名
+//         return lfetch(["{https://naiheblog.s3.bitiful.net}"+fullpath(urlPath)])//屏蔽了自带的npm劫持，改为存储桶劫持
+//     }
+//     else{
+//         return fetch(req);
+//     }
+// }
